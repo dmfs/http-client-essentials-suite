6f472bdd53132d4e71d6c3d817ba76f8c4a79f8a
Marten Gajda <marten@dmfs.org>
2016-08-20 14:37:19 +0200

    Add separate user-agent header definitions to unit tests.
    Fixes #3

--- http-executor-decorators/src/test/java/org/dmfs/httpessentials/executors/useragent/BrandedTest.java
+++ http-executor-decorators/src/test/java/org/dmfs/httpessentials/executors/useragent/BrandedTest.java
@@ -18,18 +18,18 @@
 package org.dmfs.httpessentials.executors.useragent;
 
 import org.dmfs.httpessentials.HttpMethod;
-import org.dmfs.httpessentials.client.HttpRequest;
-import org.dmfs.httpessentials.client.HttpRequestEntity;
-import org.dmfs.httpessentials.client.HttpRequestExecutor;
-import org.dmfs.httpessentials.client.HttpResponse;
-import org.dmfs.httpessentials.client.HttpResponseHandler;
+import org.dmfs.httpessentials.client.*;
+import org.dmfs.httpessentials.converters.UserAgentConverter;
 import org.dmfs.httpessentials.exceptions.ProtocolError;
 import org.dmfs.httpessentials.exceptions.ProtocolException;
 import org.dmfs.httpessentials.exceptions.RedirectionException;
 import org.dmfs.httpessentials.exceptions.UnexpectedStatusException;
+import org.dmfs.httpessentials.headers.BasicSingletonHeaderType;
 import org.dmfs.httpessentials.headers.EmptyHeaders;
 import org.dmfs.httpessentials.headers.Headers;
+import org.dmfs.httpessentials.headers.SingletonHeaderType;
 import org.dmfs.httpessentials.types.CommentedProduct;
+import org.dmfs.httpessentials.types.UserAgent;
 import org.dmfs.httpessentials.types.VersionedProduct;
 import org.junit.Test;
 
@@ -46,6 +46,13 @@ import static org.junit.Assert.assertEquals;
  */
 public class BrandedTest
 {
+
+    /**
+     * User-Agent header type.
+     */
+    private final static SingletonHeaderType<UserAgent> USER_AGENT_HEADER = new BasicSingletonHeaderType<UserAgent>(
+            "User-Agent", new UserAgentConverter());
+
 
     private class CapturingExecutor implements HttpRequestExecutor
     {
@@ -105,7 +112,7 @@ public class BrandedTest
 
         // ASSERT
         String headerString = capturingExecutor.mCapturedRequest.headers()
-                .header(UserAgentHeaderDecoration.USER_AGENT_HEADER)
+                .header(USER_AGENT_HEADER)
                 .toString();
         assertEquals("name/version (comment)", headerString);
     }
@@ -127,7 +134,7 @@ public class BrandedTest
 
         // ASSERT
         String headerString = capturingExecutor.mCapturedRequest.headers()
-                .header(UserAgentHeaderDecoration.USER_AGENT_HEADER)
+                .header(USER_AGENT_HEADER)
                 .toString();
         assertEquals("SmoothSync/1.0 (debug) smoothsync-api-client/0.4 oauth2-essentials/0.3", headerString);
     }
