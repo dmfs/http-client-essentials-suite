ad1577fb76b2504972ce3b64242c29b494310ce6
Gabor Keszthelyi <gabor.keszthelyi@gmail.com>
2016-08-30 15:42:36 +0200

    Add BottomBranded executor decorator and HttpUrlConnectionExecutor that applies the decorator by default.

---
+++ httpurlconnection-executor/src/main/java/org/dmfs/httpessentials/httpurlconnection/BottomBranded.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Marten Gajda <marten@dmfs.org>
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.httpessentials.httpurlconnection;
+
+import org.dmfs.httpessentials.client.HttpRequest;
+import org.dmfs.httpessentials.client.HttpRequestExecutor;
+import org.dmfs.httpessentials.decoration.RequestHeaderDecorator;
+import org.dmfs.httpessentials.exceptions.ProtocolError;
+import org.dmfs.httpessentials.exceptions.ProtocolException;
+import org.dmfs.httpessentials.exceptions.RedirectionException;
+import org.dmfs.httpessentials.exceptions.UnexpectedStatusException;
+import org.dmfs.httpessentials.types.Product;
+
+import java.io.IOException;
+import java.net.URI;
+
+
+/**
+ * {@link HttpRequestExecutor} decorator that adds the given {@link Product} to the end of User-Agent header of the
+ * request.
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class BottomBranded implements HttpRequestExecutor
+{
+    private final HttpRequestExecutor mExecutor;
+    private final Product mBottomProduct;
+
+
+    public BottomBranded(HttpRequestExecutor executor, Product bottomProduct)
+    {
+        mExecutor = executor;
+        mBottomProduct = bottomProduct;
+    }
+
+
+    @Override
+    public <T> T execute(URI uri, HttpRequest<T> request) throws IOException, ProtocolError, ProtocolException, RedirectionException, UnexpectedStatusException
+    {
+        return mExecutor.execute(uri,
+                new RequestHeaderDecorator<T>(request, new BottomUserAgentHeaderDecoration(mBottomProduct)));
+    }
+}
---
+++ httpurlconnection-executor/src/main/java/org/dmfs/httpessentials/httpurlconnection/HttpUrlConnectionExecutor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2016 Marten Gajda <marten@dmfs.org>
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.httpessentials.httpurlconnection;
+
+import org.dmfs.httpessentials.client.HttpRequest;
+import org.dmfs.httpessentials.client.HttpRequestExecutor;
+import org.dmfs.httpessentials.exceptions.ProtocolError;
+import org.dmfs.httpessentials.exceptions.ProtocolException;
+import org.dmfs.httpessentials.exceptions.RedirectionException;
+import org.dmfs.httpessentials.exceptions.UnexpectedStatusException;
+import org.dmfs.httpessentials.types.VersionedProduct;
+
+import java.io.IOException;
+import java.net.URI;
+
+
+/**
+ * An {@link HttpRequestExecutor} that uses Java's HttpUrlConnection (through {@link PlainHttpUrlConnectionExecutor})
+ * and appends its name and version, and the platform's description (<code>http.agent</code> system property) to
+ * existing User-Agent request headers.
+ *
+ * @author Gabor Keszthelyi
+ */
+public final class HttpUrlConnectionExecutor implements HttpRequestExecutor
+{
+    private final HttpRequestExecutor mExecutor;
+
+
+    public HttpUrlConnectionExecutor()
+    {
+        this(new PlainHttpUrlConnectionExecutor());
+    }
+
+
+    public HttpUrlConnectionExecutor(HttpUrlConnectionFactory connectionFactory)
+    {
+        this(new PlainHttpUrlConnectionExecutor(connectionFactory));
+    }
+
+
+    private HttpUrlConnectionExecutor(HttpRequestExecutor executor)
+    {
+        mExecutor = new BottomBranded(
+                new BottomBranded(executor, new VersionedProduct(BuildConfig.NAME, BuildConfig.VERSION)),
+                Platform.INSTANCE);
+    }
+
+
+    @Override
+    public <T> T execute(URI uri, HttpRequest<T> request) throws IOException, ProtocolError, ProtocolException, RedirectionException, UnexpectedStatusException
+    {
+        return mExecutor.execute(uri, request);
+    }
+}
