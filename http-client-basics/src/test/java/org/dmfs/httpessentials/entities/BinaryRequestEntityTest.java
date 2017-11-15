5d9b805d636847185eb8d993b8da752a4419db9f
Marten Gajda <marten@dmfs.org>
2017-11-15 18:36:34 +0100

    BinaryRequestEntity. Implements #104
    This adds a `BinaryRequestEntity` which takes a `Single<byte[]>`. It can be used to compose `HttpRequestEntities` for complex types which can be converted to an array of bytes.
    This also adds a `DelegatingRequestEntity` which can serve as the abstract base of such a delegating request entity.

---
+++ http-client-basics/src/test/java/org/dmfs/httpessentials/entities/BinaryRequestEntityTest.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.httpessentials.entities;
+
+import org.dmfs.httpessentials.types.MediaType;
+import org.dmfs.httpessentials.types.StringMediaType;
+import org.dmfs.httpessentials.types.StructuredMediaType;
+import org.dmfs.jems.hamcrest.matchers.PresentMatcher;
+import org.dmfs.jems.single.Single;
+import org.dmfs.jems.single.elementary.ValueSingle;
+import org.junit.Test;
+
+import java.io.OutputStream;
+
+import static org.dmfs.jems.hamcrest.matchers.PresentMatcher.isPresent;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.dummy;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.failingMock;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * @author Marten Gajda
+ */
+public class BinaryRequestEntityTest
+{
+    @Test
+    public void testContentType() throws Exception
+    {
+        assertThat(new BinaryRequestEntity(new StructuredMediaType("application", "text"), dummy(Single.class)).contentType(),
+                PresentMatcher.<MediaType>isPresent(new StringMediaType("application/text")));
+    }
+
+
+    @Test
+    public void testContentLength() throws Exception
+    {
+        assertThat(new BinaryRequestEntity(dummy(MediaType.class), new ValueSingle<>(new byte[0])).contentLength(), isPresent(0L));
+        assertThat(new BinaryRequestEntity(dummy(MediaType.class), new ValueSingle<>(new byte[1000])).contentLength(), isPresent(1000L));
+    }
+
+
+    @Test
+    public void testWriteContent() throws Exception
+    {
+        byte[] dummyData = new byte[1000];
+        OutputStream mockStream = failingMock(OutputStream.class);
+        doNothing().when(mockStream).write(dummyData);
+
+        new BinaryRequestEntity(dummy(MediaType.class), new ValueSingle<>(dummyData)).writeContent(mockStream);
+
+        // make sure the data has actually been written
+        verify(mockStream).write(dummyData);
+    }
+
+}
---
+++ http-client-basics/src/test/java/org/dmfs/httpessentials/entities/DelegatingRequestEntityTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2017 dmfs GmbH
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dmfs.httpessentials.entities;
+
+import org.dmfs.httpessentials.client.HttpRequestEntity;
+import org.dmfs.httpessentials.types.MediaType;
+import org.dmfs.optional.Optional;
+import org.junit.Test;
+
+import java.io.OutputStream;
+
+import static org.dmfs.jems.mockito.doubles.TestDoubles.dummy;
+import static org.dmfs.jems.mockito.doubles.TestDoubles.failingMock;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.verify;
+
+
+/**
+ * @author Marten Gajda
+ */
+public class DelegatingRequestEntityTest
+{
+    @Test
+    public void testContentType() throws Exception
+    {
+        HttpRequestEntity mockEntity = failingMock(HttpRequestEntity.class);
+        Optional<MediaType> dummyOptional = dummy(Optional.class);
+        doReturn(dummyOptional).when(mockEntity).contentType();
+
+        assertThat(new DelegatingTestEntity(mockEntity).contentType(), sameInstance(dummyOptional));
+    }
+
+
+    @Test
+    public void testContentLength() throws Exception
+    {
+        HttpRequestEntity mockEntity = failingMock(HttpRequestEntity.class);
+        Optional<Long> dummyOptional = dummy(Optional.class);
+        doReturn(dummyOptional).when(mockEntity).contentLength();
+
+        assertThat(new DelegatingTestEntity(mockEntity).contentLength(), sameInstance(dummyOptional));
+    }
+
+
+    @Test
+    public void testWriteContent() throws Exception
+    {
+        OutputStream dummyStream = dummy(OutputStream.class);
+
+        HttpRequestEntity mockEntity = failingMock(HttpRequestEntity.class);
+        doNothing().when(mockEntity).writeContent(dummyStream);
+
+        new DelegatingTestEntity(mockEntity).writeContent(dummyStream);
+        verify(mockEntity).writeContent(dummyStream);
+    }
+
+
+    private final static class DelegatingTestEntity extends DelegatingRequestEntity
+    {
+
+        public DelegatingTestEntity(HttpRequestEntity delegate)
+        {
+            super(delegate);
+        }
+    }
+}
